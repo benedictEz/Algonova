255d27ce13e235fd859acd64313a7a394a6a0ff1
benedictEz <benedicteoghanzhang@gmail.com>
2026-09-05 14:56:57 +0700

    class GameSprite and character

--- ping pong/main.py
+++ ping pong/main.py
@@ -1,4 +1,6 @@
 import pygame
+pygame.init()
+
 
 back = (200, 255, 255)
 win_width = 600
@@ -10,13 +12,30 @@ finish = False
 clock = pygame.time.Clock()
 FPS = 60
 
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load(player_image), (width, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
 
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+player_l = GameSprite('racket.png', 5, 200, 4, 50, 150)
+player_r = GameSprite('racket.png', 550, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 100, 200, 4, 50, 50)
 
 
 while game:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
-            break
-
+            game = False
+    window.fill(back)
+    player_l.reset()
+    player_r.reset()
+    ball.reset()
     pygame.display.update()
     clock.tick(FPS)
