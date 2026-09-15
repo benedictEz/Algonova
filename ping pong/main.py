adc0935e964eb266c69fb9bc9512eb90a73aa95a
benedictEz <benedicteoghanzhang@gmail.com>
2026-09-15 20:30:59 +0700

    adding ball and player movement

--- ping pong/main.py
+++ ping pong/main.py
@@ -21,21 +21,46 @@ class GameSprite(pygame.sprite.Sprite):
         self.rect.x = player_x
         self.rect.y = player_y
 
+    def update_r(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[pygame.K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 player_l = GameSprite('racket.png', 5, 200, 4, 50, 150)
 player_r = GameSprite('racket.png', 550, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 100, 200, 4, 50, 50)
-
+speed_x = 1
+speed_y = 1
 
 while game:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             game = False
-    window.fill(back)
+    window.fill(back)\
+    
     player_l.reset()
     player_r.reset()
+    player_l.update_l()
+    player_r.update_r()
+
     ball.reset()
+    ball.rect.x += speed_x
+    ball.rect.y += speed_y
+
+    if ball.rect.y >= 500 or ball.rect.y < 0:
+        speed_y*= -1
     pygame.display.update()
     clock.tick(FPS)
